cf652c590afc1d2f0b657d27c13c32622dfd9a21
シYou Liang <Isopach@users.noreply.github.com>
2017-01-13 11:29:58 +0900

    updated dependencies

---
+++ Acceleration Game/UnityGC/src/GameBase.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace GameCanvas
+{
+    /// <summary>
+    /// 内部に GameCanvas インスタンスを持つ MonoBehaviour
+    /// </summary>
+    public abstract class GameBase : MonoBehaviour
+    {
+        /// <summary>
+        /// GameCanvasへの参照
+        /// </summary>
+        protected GameCanvas gc;
+        
+        private void Awake()
+        {
+            gc = GameCanvas.Instance;
+            gc.ReadDataByStorage();
+
+            gc.SetOnStart(() => { enabled = true; });
+            enabled = false;
+        }
+
+        private void Update()
+        {
+            Calc();
+            Draw();
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        virtual public void Start() { }
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        virtual public void Calc() { }
+
+        /// <summary>
+        /// 描画処理
+        /// </summary>
+        virtual public void Draw() { }
+    }
+}
---
+++ Paddle Game/Assets/Scripts/Examples/CompassExample.cs
@@ -0,0 +1,30 @@
+﻿/**
+ * GameCanvas Compass Example
+ * 
+ * Copyright (c) 2016 Seibe TAKAHASHI
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+using GameCanvas;
+
+public class CompassExample : GameBase
+{
+    override public void Start()
+    {
+        // 地磁気センサーを有効化します
+        gc.isCompassEnabled = true;
+    }
+
+    override public void Draw()
+    {
+        // 画面を白で塗りつぶします
+        gc.ClearScreen();
+
+        // 背景画像を描画します
+        gc.DrawImage(0, 0, 0);
+
+        // ボールを描画します
+        gc.DrawRotatedImage(1, 308, 228, gc.compass, 12, 12);
+    }
+}
